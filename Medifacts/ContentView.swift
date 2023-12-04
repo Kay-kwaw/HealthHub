c727dfbaa157b5c79c608ffba5099bb9eaa05896
kwaw kumi <kwawkumi9@gmail.com>
2023-12-04 10:41:52 +0000

    Initial Commit

---
+++ Medifacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Medifacts
+//
+//  Created by kwaw-kumi on 04/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Medifacts/MedifactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedifactsApp.swift
+//  Medifacts
+//
+//  Created by kwaw-kumi on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MedifactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
